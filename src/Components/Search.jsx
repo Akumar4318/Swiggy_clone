0a473f9e3661acf02a105c92896db7582f1ae581
Akumar4318 <akumar4318@gmail.com>
2025-01-10 14:16:16 +0530

    Search Craosel

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,51 +1,92 @@
 import { useEffect, useState } from "react";
-import Navbar from "./Navbar"
+import Navbar from "./Navbar";
 import { CiSearch } from "react-icons/ci";
 import SuggestionBox from "./SuggestionBox";
 import { CloudinaryURL } from "../Utils/Link";
+import SearchCraousel from "./SearchCraousel";
 
 const Search = () => {
+  // State for carousel data
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("");
+  const [suggestion, setSuggestion] = useState([]);
 
-  
-const[query,setQuery]=useState('');
-const[suggestion,setSuggestion]=useState([])
+  // Fetch carousel data
+  useEffect(() => {
+    const fetchDetails = async () => {
+      const response = await fetch(
+        "https://www.swiggy.com/dapi/landing/PRE_SEARCH?lat=28.6489049&lng=77.271046"
+      );
+      const result = await response.json();
+      setData(result.data.cards[1].card.card.gridElements.infoWithStyle.info);
+    };
+    fetchDetails();
+  }, []);
 
-useEffect(()=>{
-  const getalldata= async ()=>{
-    let response=await fetch(`https://www.swiggy.com/dapi/restaurants/search/suggest?lat=28.6489049&lng=77.271046&str=${query}&trackingId=undefined&includeIMItem=true`)
-    
-    let data=await response.json();
-    console.log(data)
-    setSuggestion(data.data.suggestions)
-    
-  }
- getalldata()
-},[query])
+  // Fetch suggestions based on user input
+  useEffect(() => {
+    if (!query.trim()) {
+      setSuggestion([]);
+      return;
+    }
+
+    const getAllData = async () => {
+      const response = await fetch(
+        `https://www.swiggy.com/dapi/restaurants/search/suggest?lat=28.6489049&lng=77.271046&str=${query}&trackingId=undefined&includeIMItem=true`
+      );
+      const result = await response.json();
+      setSuggestion(result.data.suggestions);
+    };
+    getAllData();
+  }, [query]);
+
   return (
     <div>
-        <Navbar/>
-        <div className="w-3/4  mt-[48px] mb-[16px] mx-auto px-[256px]">
-            <div className="  border flex items-center justify-center  rounded-md p-1">
-            <input onChange={(e)=>{
-              if(e.target.value==""){
-                setSuggestion([])
-              }
-              setQuery(e.target.value)
-            }} type="text" className=" w-[100%]   p-4 font-gilory  focus:outline-none focus:ring-0   " placeholder='search for restaurants' />
-            <CiSearch  className="w-[30px] h-[30px]  mr-5"/>
-            </div>
-           <div className="overflow-hidden flex flex-col gap-4 rounded mt-3">
-           {
-           suggestion ? (suggestion.map((item,index)=>{
-            
-            return <SuggestionBox key={index} name={item.text} subCategory={item.subCategory} img={CloudinaryURL +item.cloudinaryId}/>
-           })):([])
-           }
-           </div>
-           
+      <Navbar />
+      <div className="w-3/4 mt-[48px] mb-[16px] mx-auto px-[256px]">
+        {/* Search Bar */}
+        <div className="border flex items-center justify-center rounded-md p-1">
+          <input
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            type="text"
+            className="w-[100%] p-4 font-gilory focus:outline-none focus:ring-0"
+            placeholder="Search for restaurants"
+          />
+          <CiSearch className="w-[30px] h-[30px] mr-5" />
         </div>
-    </div>
-  )
-}
 
-export default Search
+        {/* Carousel */}
+        {query.trim() === "" && (
+          <div className="mt-8 cursor-pointer">
+            <p className="font-Novabold text-[20px]">Popular Cuisines</p>
+            <div className="flex overflow-x-auto space-x-4 mx-2 mt-3 no-scrollbar">
+              {data &&
+                data.map((item, index) => (
+                  <SearchCraousel
+                    key={index}
+                    img={CloudinaryURL + item.imageId}
+                  />
+                ))}
+            </div>
+          </div>
+        )}
+
+        {/* Suggestions */}
+        <div className=" flex flex-col gap-4 rounded mt-3">
+          {suggestion &&
+            suggestion.map((item, index) => (
+              <SuggestionBox
+                key={index}
+                name={item.text}
+                subCategory={item.subCategory}
+                img={CloudinaryURL + item.cloudinaryId}
+              />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
---
+++ src/Components/SearchCraousel.jsx
@@ -0,0 +1,14 @@
+
+
+const SearchCraousel = ({img}) => {
+   
+  return (
+    <div className="flex  ">
+       
+<img src={img} alt="" className="w-[100px] h-[100px]" />
+
+    </div>
+  )
+}
+
+export default SearchCraousel
